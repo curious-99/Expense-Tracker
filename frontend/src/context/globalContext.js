e57a4a3741a62420c2848e1170982bfe7fb39148
curious-99 <mohit39.ugcs20@iiitranchi.com>
2023-11-06 11:03:47 +0530

    hostedBackend

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:8000/api/v1/";
+const BASE_URL = "https://expensetracker-zste.onrender.com/api/v1/";
 
 
 const GlobalContext = React.createContext()
